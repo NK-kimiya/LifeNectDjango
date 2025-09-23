47203c28b9b4ad9663af90a31ea4728e8a24363c
NK-kimiya <kinarishige26@gmail.com>
2025-09-23 13:05:54 +0900

    JWTトークンを最小限で実装

--- myproject/settings.py
+++ myproject/settings.py
@@ -37,10 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+     "rest_framework",
+    "corsheaders",
     "myapp",
 ]
 
 MIDDLEWARE = [
+     "corsheaders.middleware.CorsMiddleware",  
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,6 +52,20 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+    # 開発開始直後はAPIを叩きやすいようAllowAnyにしてもOK
+    # "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.AllowAny",)
+    # 運用寄りはIsAuthenticated
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+}
 
 ROOT_URLCONF = 'myproject.urls'
 
