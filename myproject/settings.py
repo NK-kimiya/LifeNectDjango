22de4d534e1fc2777c453cdbbad8597d94bf7f21
NK-kimiya <kinarishige26@gmail.com>
2026-03-23 11:21:43 +0900

    setting.py change debug mode

--- myproject/settings.py
+++ myproject/settings.py
@@ -48,8 +48,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mmr=+bo!)hmnfs4ll)84!#v(*m$0$1z1+@kjr@#x04(gl!*8#$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = os.environ.get("DEBUG", "False") == "True"
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = [".onrender.com", "localhost", "127.0.0.1"]
 
