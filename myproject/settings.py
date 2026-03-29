840f29052c9928db481ad845861d923adde30f89
NK-kimiya <kinarishige26@gmail.com>
2026-03-29 12:48:28 +0900

    reset secret-key hide secret-key

--- myproject/settings.py
+++ myproject/settings.py
@@ -45,7 +45,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-mmr=+bo!)hmnfs4ll)84!#v(*m$0$1z1+@kjr@#x04(gl!*8#$'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
