48516258c5ae05bf2fd3322cccb2259d4ee9f987
NK-kimiya <kinarishige26@gmail.com>
2026-09-19 19:52:11 +0900

    AIチャットを別ページに移行

--- myapp/views/me.py
+++ myapp/views/me.py
@@ -3,7 +3,8 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from myapp.permissions import IsActiveAccount
 from myapp.serializers.user import UserProfileSerializer
-
+from myapp.services.cloudflare_r2 import delete_object, CloudflareR2Error
+from rest_framework import status
 
 class MeView(APIView):
     permission_classes = [IsAuthenticated,IsActiveAccount]
@@ -23,3 +24,20 @@ class MeView(APIView):
         serializer.save()
 
         return Response(serializer.data)
+
+def delete(self, request):
+    user = request.user
+    avatar_key = user.avatar_key
+
+    user.delete()
+
+    if avatar_key:
+        try:
+            delete_object(avatar_key)
+        except CloudflareR2Error:
+            pass
+
+    return Response(
+        {"detail": "アカウントを削除しました。"},
+        status=status.HTTP_200_OK,
+    )
